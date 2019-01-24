20f2ee673906a3f66ae052abb5145b89388de99e
johnnyhperkins <johnnyhperkins@gmail.com>
2019-01-24 13:37:03 -0500

    Link splash image to s3 bucket

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => (
     <footer className="flex-container">
         <section>
-          <p>&copy; 2019 A/a FullStack</p>
-          <p><Link to="https://github.com/johnnyhperkins">Github</Link>&nbsp;&bull;&nbsp;
-          <Link to="https://honestwebsitedesign.com">Portfolio</Link>&nbsp;&bull;&nbsp;
-          <Link to="https://www.linkedin.com/in/john-h-perkins/">LinkedIn</Link></p>
+          <p>Johnny Perkins</p>
+          <p><a href="mailto:johnnyhperkins@gmail.com">Email</a>&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+          <a href="https://github.com/johnnyhperkins">Github</a>&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+          <a href="https://honestwebsitedesign.com">Portfolio</a>&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+          <a href="https://www.linkedin.com/in/john-h-perkins/">LinkeddIn</a></p>
         </section>
         <section>
-          <p>Johnny Perkins</p>
-          <p><a href="mailto:johnnyhperkins@gmail.com">Email</a></p>
         </section>
-        <section></section>
+        <section>
+          <p>&copy; 2019 A/a FullStack</p>
+        </section>
     </footer>
 );
 
