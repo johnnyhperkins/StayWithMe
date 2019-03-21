404467473a8bf8c6bbd20012f5444d2133021b40
johnnyhperkins <johnnyhperkins@gmail.com>
2019-03-21 14:31:56 -0400

    Fix marker window images

--- frontend/components/search/marker_content.jsx
+++ frontend/components/search/marker_content.jsx
@@ -5,7 +5,7 @@ const MarkerContent = ({ listing, home_types }) => {
   const { photos, price, title, home_type_id, id } = listing;
   return (
     <div className="marker-window">
-      <InfoWindowSlider listing_id={id} photos={photos.slice(0,1)} className="results-slider" />
+      <div style={{'backgroundImage': `url(${photos[0]})`}} className="listing-thumb" />
       <div className="marker-window__details">
         {Object.values(home_types).filter(ht => ht.id == home_type_id).map(ht => <h6 key={ht.id} className="text--light-gray">{ht.name}</h6>)}
         <h5>{title}</h5>
