dc4c05fd9f5e0d5668133ada2a9977bf8fe01b74
johnnyhperkins <johnnyhperkins@gmail.com>
2019-03-21 14:46:12 -0400

    Fix marker window link

--- frontend/components/search/marker_content.jsx
+++ frontend/components/search/marker_content.jsx
@@ -5,7 +5,7 @@ const MarkerContent = ({ listing, home_types }) => {
   const { photos, price, title, home_type_id, id } = listing;
   return (
     <div className="marker-window">
-      <div style={{'backgroundImage': `url(${photos[0]})`}} className="listing-thumb" />
+      <a href={`/#/listings/${id}`} target="_blank" style={{'backgroundImage': `url(${photos[0]})`}} className="listing-thumb" />
       <div className="marker-window__details">
         {Object.values(home_types).filter(ht => ht.id == home_type_id).map(ht => <h6 key={ht.id} className="text--light-gray">{ht.name}</h6>)}
         <h5>{title}</h5>
